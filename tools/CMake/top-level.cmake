b9563e62019c09a0bdf33d977ceb6aed782ee29c
Jonas Claeson <jonas.cl@protonmail.com>
2018-11-01 11:20:27 +0100

    Add top-level CMake file

---
+++ tools/CMake/top-level.cmake
@@ -0,0 +1,20 @@
+#
+# Copyright 2018, Mokshasoft AB (mokshasoft.com)
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+#
+# This file should be copied/symlinked into the top level of a project.
+# See https://github.com/mokshasoft/bare-metal-idris-manifest
+#
+
+# Add all directories needed to build the Idris apps
+add_subdirectory(starterwarefree)
+add_subdirectory(starterwarefree-idris-ffi)
+add_subdirectory(libsel4-idris-rts)
+add_subdirectory(bare-metal-idris)
